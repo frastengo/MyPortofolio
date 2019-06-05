419bd9098b811d89775643673b3a38e6eb28c7dd
Francisca <frastengo@gmail.com>
2019-06-05 09:36:03 -0700

    added build path in index

--- server/index.js
+++ server/index.js
@@ -3,5 +3,10 @@ const app = express()
 
 // const {PORT} = process.env
 
+const path = require('path')
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
+
 const PORT = 4001;
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
