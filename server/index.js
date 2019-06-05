3f7a3eb3919033002cb0cdeaac902ee0461ccd28
Francisca <frastengo@gmail.com>
2019-06-05 12:02:47 -0700

    added express static

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express()
 
 // const {PORT} = process.env
 
+app.use( express.static( `${__dirname}/../build` ) );
+
 const path = require('path')
 app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, '../build/index.html'));
