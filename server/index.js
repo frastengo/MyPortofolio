d72670a74eb29ee32bf03fa3ab79ffb088038c23
Francisca <frastengo@gmail.com>
2019-06-05 18:42:07 -0700

    another build with const index

--- server/index.js
+++ server/index.js
@@ -6,14 +6,14 @@ const path = require('path')
 
 app.use( express.static( `${__dirname}/../build` ) );
 
-app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname, '../build/index.html'));
-})
+// app.get('*', (req, res)=>{
+//   res.sendFile(path.join(__dirname, '../build/index.html'));
+// })
 
-// app.get('*', function (req, res) {
-//     const index = path.join(__dirname, 'build', 'index.html');
-//     res.sendFile(index);
-//   });
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
 
 const PORT = 4001;
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
