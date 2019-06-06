e30fb4d1902ad0c3e675ef5082e56cc71c29f345
Francisca <frastengo@gmail.com>
2019-06-06 09:14:23 -0700

    fixed path build in index

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-const path = require('path')
+
 
 app.use( express.static( `${__dirname}/../build` ) );
 
+const path = require('path')
 app.get('*', (req, res)=>{
   res.sendFile(path.join(__dirname, '../build/index.html'));
 })
