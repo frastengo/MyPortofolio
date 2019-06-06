2606a06d2c51197436c8963d2d223d328c083d99
Francisca <frastengo@gmail.com>
2019-06-05 18:51:30 -0700

    deleted envdevelopment

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
-const path = require('path')
 
 // const {PORT} = process.env
 
 app.use( express.static( `${__dirname}/../build` ) );
 
 // app.get('*', (req, res)=>{
-//   res.sendFile(path.join(__dirname, '../build/index.html'));
-// })
-
+    //   res.sendFile(path.join(__dirname, '../build/index.html'));
+    // })
+    
+    const path = require('path')
 app.get('*', function (req, res) {
     const index = path.join(__dirname, 'build', 'index.html');
     res.sendFile(index);
