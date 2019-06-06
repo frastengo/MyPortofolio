6955c2b0edb648d1033a73841269f1103c9ec1e7
Francisca <frastengo@gmail.com>
2019-06-05 18:59:13 -0700

    trying client/build

--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const app = express()
 
+const path = require('path')
 // const {PORT} = process.env
 
-app.use( express.static( `${__dirname}/../build` ) );
+// app.use( express.static( `${__dirname}/../build` ) );
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // app.get('*', (req, res)=>{
     //   res.sendFile(path.join(__dirname, '../build/index.html'));
     // })
     
-    const path = require('path')
-app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'build', 'index.html');
-    res.sendFile(index);
-  });
+// app.get('*', function (req, res) {
+//     const index = path.join(__dirname, 'build', 'index.html');
+//     res.sendFile(index);
+//   });
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const PORT = 4001;
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
