f24c2e61e731557be381f4c099052875924e3861
Francisca <frastengo@gmail.com>
2019-06-05 19:18:14 -0700

    regular

--- server/index.js
+++ server/index.js
@@ -1,24 +1,12 @@
 const express = require('express')
 const app = express()
-
 const path = require('path')
-// const {PORT} = process.env
 
-// app.use( express.static( `${__dirname}/../build` ) );
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use( express.static( `${__dirname}/../build` ) );
 
-// app.get('*', (req, res)=>{
-    //   res.sendFile(path.join(__dirname, '../build/index.html'));
-    // })
-    
-// app.get('*', function (req, res) {
-//     const index = path.join(__dirname, 'build', 'index.html');
-//     res.sendFile(index);
-//   });
-
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
 
 const PORT = 4001;
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
