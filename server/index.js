ee714ca105ac91829556974a8b784a9040d74a20
Francisca <frastengo@gmail.com>
2019-06-05 18:30:56 -0700

    added const index on path

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 // const {PORT} = process.env
 
 app.use( express.static( `${__dirname}/../build` ) );
 
-const path = require('path')
-app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname, '../build/index.html'));
-})
+// app.get('*', (req, res)=>{
+//   res.sendFile(path.join(__dirname, '../build/index.html'));
+// })
+
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
 
 const PORT = 4001;
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
