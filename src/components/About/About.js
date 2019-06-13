e9069846396578647005b3028cd5283e71e36cf4
Francisca <frastengo@gmail.com>
2019-06-13 13:55:39 -0700

    fixed about me - changed picture size and paragraph

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,16 +13,18 @@ function About () {
                 </div> */}
                 {/* <h1>{front + " " + "Hello & Welcome! " + back }</h1> */}
                 <div className='p-image'>
-                    <img src={imageMe}/>
-                    <p>I’m originally from <span>Chile </span>and have been living in the United States for about 16 years. I am proficient in both English and Spanish. My professional career was focused on the health industry which led me to positions that strengthen my customer service skills, time management, and proficiency. 
+                    
+                    <p>Motivated and passionate bilingual professional with a full year of experience in web developing and significant background in customer service experience that is looking for an exciting opportunity to offer strong coding skills and continue to thrive as a<span> Web Developer.</span> 
+                        
                     </p>
                 </div>
+                    <img src={imageMe}/>
                 
                     <br/>
-                <p>
-                    During that journey, I realized that I was very interested and enjoyed the use of technology and computer software which is what trigger the decision to start exploring <span>coding</span>. I started with resources online that then led me to a three month program from Code Academy, Build Websites From Scratch. This amazing experience made me realized that I wanted to become a <span>Web Developer</span> and it is why I decided to attend DevMountain coding boot camp. I absolutely loved it and I truly believed I have found my passion. I am now ready to continue to explore, learn, and <span> expand my skills.</span>
+                {/* <p>
+                    During that journey, I realized that I was very interested and enjoyed the use of technology and computer software which is what trigger the decision to start exploring <span>coding</span>. I started with resources online that then led me to a three month program from Code Academy, Build Websites From Scratch. This amazing experience made me realized that I wanted to become a <span>Web Developer</span> and it is why I decided to attend DevMountain coding boot camp. I absolutely loved it and I truly believed I have found my passion. I am now ready to continue to explore, learn, and <span> expand my skills.</span> */}
 
-                </p>
+                {/* </p> */}
                 
             </div>
         </div>
